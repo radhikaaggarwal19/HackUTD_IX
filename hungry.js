cad51377fda675e92ffb2d4a37b2d5a49f759f56
Radhika Aggarwal <radhikaxaggarwal@gmail.com>
2022-11-13 11:52:42 -0600

    Updates: speed & type check functionalities

--- hungry.js
+++ hungry.js
@@ -3,6 +3,8 @@ let map = null;
 let mapOption = null;
 let radiusOption = null;
 
+let pos;
+
 function myFunction() {
     mapOption = document.getElementById("typeOption").value;
     radiusOption = document.getElementById("radiusOption").value;
@@ -16,13 +18,40 @@ function myFunction() {
     initMap();
 }
 
+function distance(lat1,
+    lat2, lon1, lon2) {
+
+    // The math module contains a function
+    // named toRadians which converts from
+    // degrees to radians.
+    lon1 = lon1 * Math.PI / 180;
+    lon2 = lon2 * Math.PI / 180;
+    lat1 = lat1 * Math.PI / 180;
+    lat2 = lat2 * Math.PI / 180;
+
+    // Haversine formula
+    let dlon = lon2 - lon1;
+    let dlat = lat2 - lat1;
+    let a = Math.pow(Math.sin(dlat / 2), 2)
+        + Math.cos(lat1) * Math.cos(lat2)
+        * Math.pow(Math.sin(dlon / 2), 2);
+
+    let c = 2 * Math.asin(Math.sqrt(a));
+
+    // Radius of earth in kilometers. Use 3956
+    // for miles
+    let r = 6371;
+
+    // calculate the result
+    return (c * r);
+}
 
 function initMap() {
 
     let location = new Object();
     navigator.geolocation.getCurrentPosition(function (position) {
 
-        var pos = {
+        pos = {
             lat: position.coords.latitude,
             lng: position.coords.longitude
         };
@@ -43,37 +72,76 @@ function getRestaurants(location, mapOption, radiusOption) {
         radius: radiusOption,
         type: [mapOption],
         openNow: true,
-        rankby: 'distance'
     };
     service = new google.maps.places.PlacesService(map);
     service.nearbySearch(request, callback);
 }
 
 function callback(results, status) {
-    console.log(status);
     if (status == google.maps.places.PlacesServiceStatus.OK) {
-        console.log(status);
         for (var i = 0; i < results.length; i++) {
-            console.log(results);
-            var place = results[i];
-            let price = createPrice(place.price_level);
-            let content = `<h3>${place.name}</h3>
-            <h4>${place.vicinity}</h4>
-            <p>Price: ${price}</br>
-            Rating: ${place.rating}`;
+            if (radiusOption == '4827' && distance(results[i].geometry.location['lat'](), pos.lat, results[i].geometry.location['lng'](), pos.lng) >= 2) {
+                var place = results[i];
+                let price = createPrice(place.price_level);
+                let content = `<h3>${place.name}</h3>
+                <h4>${place.vicinity}</h4>
+                <p>Price: ${price}</br>
+                Rating: ${place.rating}`;
 
-            var marker = new google.maps.Marker({
-                position: place.geometry.location,
-                map: map,
-                title: place.name
-            });
+                var marker = new google.maps.Marker({
+                    position: place.geometry.location,
+                    map: map,
+                    title: place.name
+                });
 
-            var infowindow = new google.maps.InfoWindow({
-                content: content
-            });
+                var infowindow = new google.maps.InfoWindow({
+                    content: content
+                });
 
-            bindInfoWindow(marker, map, infowindow, content);
-            marker.setMap(map);
+                bindInfoWindow(marker, map, infowindow, content);
+                marker.setMap(map);
+            } else if (radiusOption == '3218' && distance(results[i].geometry.location['lat'](), pos.lat, results[i].geometry.location['lng'](), pos.lng) >= 1) {
+                var place = results[i];
+                let price = createPrice(place.price_level);
+                let content = `<h3>${place.name}</h3>
+                <h4>${place.vicinity}</h4>
+                <p>Price: ${price}</br>
+                Rating: ${place.rating}`;
+
+                var marker = new google.maps.Marker({
+                    position: place.geometry.location,
+                    map: map,
+                    title: place.name
+                });
+
+                var infowindow = new google.maps.InfoWindow({
+                    content: content
+                });
+
+                bindInfoWindow(marker, map, infowindow, content);
+                marker.setMap(map);
+            } else if (radiusOption == '1609') {
+                var place = results[i];
+                let price = createPrice(place.price_level);
+                let content = `<h3>${place.name}</h3>
+                <h4>${place.vicinity}</h4>
+                <p>Price: ${price}</br>
+                Rating: ${place.rating}`;
+
+                var marker = new google.maps.Marker({
+                    position: place.geometry.location,
+                    map: map,
+                    title: place.name
+                });
+
+                var infowindow = new google.maps.InfoWindow({
+                    content: content
+                });
+
+                bindInfoWindow(marker, map, infowindow, content);
+                marker.setMap(map);
+            }
+
 
         }
     }
